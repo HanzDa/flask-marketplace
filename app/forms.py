ad8b7418cfa16d3c9254c3cbadbed5ce3e5cc7dd
HanzDa <hdgl.9914@gmail.com>
2022-01-06 23:48:19 -0500

    working in register styles

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TelField, BooleanField
 
+
+
 class SignUpForm(FlaskForm):
-    names = StringField(label='Names')
-    surnames = StringField(label='Surnames')
+    fulname = StringField(label='Full name')
     phoneNumber = TelField(label='Phone Number')
     residenceAddress = StringField(label='Residence Address')
     username = StringField(label='Username')
--- app/views.py
+++ app/views.py
@@ -3,12 +3,16 @@ from flask import render_template
 
 from app.forms import SignUpForm
 
+
 @app.route('/')
 @app.route('/home/')
 def index():
     return render_template('base.html', show_navbar=True, show_footer=True)
 
-@app.route('/regiter', methods=['GET', 'POST'])
+@app.route('/register/', methods=['GET', 'POST'])
 def regiter():
     form = SignUpForm()
+    if form.validate_on_submit():
+        name = form.names.data
+        
     return render_template('user/register.html', form = form)
